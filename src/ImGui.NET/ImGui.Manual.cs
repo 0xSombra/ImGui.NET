22251fadd4be351aafc7499b93fe6e00f262afa2
Sombra <26506177+Sombra0xCC@users.noreply.github.com>
2021-05-07 01:34:49 +0200

    add BeginPopupModal(name, flags)

--- src/ImGui.NET/ImGui.Manual.cs
+++ src/ImGui.NET/ImGui.Manual.cs
@@ -8,6 +8,35 @@ namespace ImGuiNET
 {
     public static unsafe partial class ImGui
     {
+        public static bool BeginPopupModal(string name, ImGuiWindowFlags flags)
+        {
+            byte* native_name;
+            int name_byteCount = 0;
+            if (name != null)
+            {
+                name_byteCount = Encoding.UTF8.GetByteCount(name);
+                if (name_byteCount > Util.StackAllocationSizeLimit)
+                {
+                    native_name = Util.Allocate(name_byteCount + 1);
+                }
+                else
+                {
+                    byte* native_name_stackBytes = stackalloc byte[name_byteCount + 1];
+                    native_name = native_name_stackBytes;
+                }
+                int native_name_offset = Util.GetUtf8(name, native_name, name_byteCount);
+                native_name[native_name_offset] = 0;
+            }
+            else { native_name = null; }
+            byte* p_open = null;
+            byte ret = ImGuiNative.igBeginPopupModal(native_name, p_open, flags);
+            if (name_byteCount > Util.StackAllocationSizeLimit)
+            {
+                Util.Free(native_name);
+            }
+            return ret != 0;
+        }
+
         public static bool InputText(
             string label,
             byte[] buf,
