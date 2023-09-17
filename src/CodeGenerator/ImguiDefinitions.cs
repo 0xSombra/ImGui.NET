2bc34a487abdf452238552b98693bfbdfe257be2
0xSombra <26506177+0xSombra@users.noreply.github.com>
2023-09-17 21:46:18 +0300

    generator: add imgui ClearActiveID internal function

--- src/CodeGenerator/ImguiDefinitions.cs
+++ src/CodeGenerator/ImguiDefinitions.cs
@@ -22,6 +22,7 @@ namespace CodeGenerator
             "igGetHoveredID",
             "igSetHoveredID",
             "igGetItemID",
+            "igClearActiveID",
 
             /* ======== Docking ======== */
             "igDockContextInitialize",
